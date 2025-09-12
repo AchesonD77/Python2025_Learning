235b8bc5bd900e789e542531ea57020cfaac30b6
AchesonDan <achesondan@163.com>
2025-09-12 19:46:48 +0200

    learn some methods and arithmetic operators of numbers, and the math module, 250912

--- HelloWord/arithmetic_operations_08.py
+++ HelloWord/arithmetic_operations_08.py
@@ -7,4 +7,22 @@ print(10 / 3)
 print(10 // 3)
 print(10 % 3)
 
-# augmented
+print('————————————————')
+# augmented assignment operator
+x = 10
+x = x + 3
+x += 3
+x -= 3
+print(x)
+
+# higher precedence
+x = 10 + 3 * 2 ** 2
+print(x)
+x_2 = (2 + 3) * 10 - 3
+print(x_2)
+
+# the sequence
+# parenthesis()
+# exponentiation 2**3
+# multiplication or division
+# addition or subtraction
---
+++ HelloWord/functions_working_on_numbers_9.py
@@ -0,0 +1,18 @@
+
+
+# functions working with numbers
+x = 2.9
+# round function
+print(round(x))
+# abs function, return positive numbers
+x_2 = -2.9
+print(abs(x_2))
+
+# a sea of functions for working with numbers, complex mathmatical calculations, import a math module
+# module in Peython is a separate file with some reusable code
+import math
+# math. to call the functions.
+print(math.ceil(5.4))
+print(math.floor(5.9))
+
+# how to learn the functions: Google Python 3 math modules
