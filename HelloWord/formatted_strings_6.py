ef54cafe1889a8bfcade0bdc41066c2582ee3578
AchesonDan <achesondan@163.com>
2025-09-14 13:13:12 +0200

    learn if statement, logical operators, comparison operators, while loops, and did some related exercise  250914

--- HelloWord/formatted_strings_6.py
+++ HelloWord/formatted_strings_6.py
@@ -1,8 +1,8 @@
 # formatted_strings -> dynamically generate some texts with your variables.
-# let's two parts of name we have
+# let's say the  two parts of name we have
 name = 'acheson'
 first_name = 'dan'
-# not good method
+# not a good method
 message = name + ' [' + first_name +'] is a coder'
 # prefixed with f, formatted string, use curly braces to dynamically insert values into your strings.
 # mes = f'{}'
---
+++ HelloWord/while_loops_14.py
@@ -0,0 +1,33 @@
+# while loops, excute a block of codes multiple times.
+# while method, basical of while
+count_number = 1
+while count_number <= 5:
+    print(count_number)
+    count_number = count_number + 1
+print("Done")
+# a simple game
+count_number = 1
+while count_number <= 5:
+    print("*" * count_number)
+    count_number = count_number + 1
+print("Done")
+
+# A guess game
+secret_number = 9
+# rename all the characters
+count_number = 0
+guess_limit = 3
+while count_number < guess_limit:
+    guess = int(input(f"Guess: "))
+    count_number += 1
+    if count_number == (guess_limit-1):
+        print("Last chance")
+    if guess == secret_number:
+        print("You won!")
+        # break: terminate the loop
+        break
+else:
+    print("Sorry, you faild!")
+
+
+
