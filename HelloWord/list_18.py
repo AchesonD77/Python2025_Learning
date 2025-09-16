cede89f78f36fc84d1ce9ba751eeb94f8f56bd28
AchesonDan <achesondan@163.com>
2025-09-16 12:43:48 +0200

    did a car game with while, if, break, not; learn for loops; learn nested loops and make an exercise, learn list. 20250916 1.4h

---
+++ HelloWord/list_18.py
@@ -0,0 +1,13 @@
+# list
+# 1. list of name
+names = ['Dan', 'J', 'Patrick', 'W', 'F']
+# 2. using index in list to find a specific/particulare value, starting from 0. -1 means first item end of the list.
+# 3. don't change the list, creat a new one.
+names[0] = 'D'
+print(names[2])
+print(names[-2])
+print(names[2:])
+print(names[2:4]) # 'p' 'w'
+print(names)
+
+# 4. write a program to find the largest number in a list
