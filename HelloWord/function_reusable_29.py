ba52631f73bb0ec01d9e129b28d6b6eae481ddd3
AchesonDan <achesondan@163.com>
2025-09-17 16:45:01 +0200

    learned about how to define functions, how to pass information to functions, how to return values from functions, to reusable functions, and how to handle exceptions. 20250917 1h30m

---
+++ HelloWord/function_reusable_29.py
@@ -0,0 +1,33 @@
+# creating reusable functions
+# reorganize the dictionary_emoji_convert_exercise_25.py to be a new reusable function
+# we don't set up input and output, because maybe they have different ways.
+# each function can only work on one specific task, and we neet to show the task through our define name
+
+# define it
+def emoji_convert(message):
+    # 2. split("")
+    # message.split(" "), method uses " " to seperate the string
+    words = message.split(" ")
+    print(words)
+
+    # 3. define a dictionary mapping, mac : control + command + space
+    emojis = {
+        ":)": "😜",
+        ":(": "😞"
+    }
+    # output
+    output = ""
+
+    # 4. for loop to see if we have the items to map the key
+    # we use get() method again: get(word, word), to return we input but don't have in our dictionary
+    for word in words:
+        output += emojis.get(word, word) + " "
+    # return statement
+    return output
+# remember after define a function, we need to make two blank lines
+
+
+# call it
+message = input(">")
+print(emoji_convert(message))
+
